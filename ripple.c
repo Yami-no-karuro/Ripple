ea7f9cb005cb8ab9fe0df8ff50133561cca65ff9
Yami-no-karuro <carloborgna98@gmail.com>
2026-04-02 18:02:56 +0200

    Working version

--- ripple.c
+++ ripple.c
@@ -5,7 +5,9 @@
 #include <string.h>
 #include <fcntl.h>
 #include <dirent.h>
+#include <errno.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 
 #define FILE_BUF_SIZE 4096
 #define PATH_BUF_SIZE 256
@@ -15,7 +17,25 @@ typedef struct {
     const char *dst;
 } args_t;
 
-int copy_file(const char *src_path, const char *dst_path) 
+int parse_args(int argc, char *argv[], args_t *args)
+{
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <src> <dst>\n", argv[0]);
+        return -1;
+    }
+
+    struct stat stat_buff;
+    if (stat(argv[1], &stat_buff) != 0 || S_ISDIR(stat_buff.st_mode)) {
+        fprintf(stderr, "[!] Invalid source path '%s'\n", argv[1]);
+        return -1;
+    }
+    
+    args->src = argv[1];
+    args->dst = argv[2];
+    return 0;
+}
+
+int copy_file(const char *src_path, const char *dst_path)
 {
     FILE *src = fopen(src_path, "rb");
     if (!src) {
@@ -24,15 +44,14 @@ int copy_file(const char *src_path, const char *dst_path)
     }
 
     FILE *dst = fopen(dst_path, "wb");
-    if (!dst) { 
+    if (!dst) {
         fprintf(stderr, "[!] Unable to open file '%s' for writing\n", dst_path);
-        fclose(src); 
-        return -1; 
+        fclose(src);
+        return -1;
     }
 
     unsigned char buf[FILE_BUF_SIZE];
     size_t n;
-
     while ((n = fread(buf, 1, FILE_BUF_SIZE, src)) > 0) {
         if (fwrite(buf, 1, n, dst) != n) {
             fprintf(stderr, "[!] Unable to write to file '%s'\n", dst_path);
@@ -50,6 +69,13 @@ int copy_file(const char *src_path, const char *dst_path)
 
 int copy_directory(const char *src_path, const char *dst_path)
 {
+    if (mkdir(dst_path, 0755) != 0) {
+        if (errno != EEXIST) {
+            fprintf(stderr, "[!] Unable to create directory '%s'\n", dst_path);
+            return -1;
+        }
+    }
+
     DIR *dir = opendir(src_path);
     if (NULL == dir) {
         fprintf(stderr, "[!] Unable to open directory '%s'\n", src_path);
@@ -66,37 +92,27 @@ int copy_directory(const char *src_path, const char *dst_path)
 
         snprintf(src_entry_path, PATH_BUF_SIZE, "%s/%s", src_path, entry->d_name);
         snprintf(dst_entry_path, PATH_BUF_SIZE, "%s/%s", dst_path, entry->d_name);
-
         printf("%s -> %s\n", src_entry_path, dst_entry_path);
+
         switch (entry->d_type) {
-            DT_DIR:
-                copy_directory(src_entry_path, dst_entry_path);
+            case DT_DIR:
+                if (copy_directory(src_entry_path, dst_entry_path) != 0) {
+                    closedir(dir);
+                    return -1;
+                }
+
                 break;
             default:
-                copy_file(src_entry_path, dst_entry_path);
+                if (copy_file(src_entry_path, dst_entry_path) != 0) {
+                    closedir(dir);
+                    return -1;
+                }
+
+                break;
         }
     }
 
-    return 0;
-}
-
-int parse_args(int argc, char *argv[], args_t *args) 
-{
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <src> <dst>\n", argv[0]);
-        return -1;
-    }
-
-    struct stat stat_buff;
-    for (size_t i = 1; i < argc; i++) {
-        if (stat(argv[i], &stat_buff) != 0) {
-            fprintf(stderr, "[!] Invalid path '%s'\n", argv[i]);
-            return -1;
-        }
-    }
-
-    args->src = argv[1];
-    args->dst = argv[2];
+    closedir(dir);
     return 0;
 }
 
@@ -106,6 +122,5 @@ int main(int argc, char *argv[])
     if (parse_args(argc, argv, &args))
         return -1;
 
-    copy_directory(args.src, args.dst);
-    return 0;
+    return copy_directory(args.src, args.dst);
 }
